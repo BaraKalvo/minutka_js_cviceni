bff1e6ac4ea4a570deb28eb79f1fbbfc4d5071d6
Barbora Kalvodová <kalvodova.bara@seznam.cz>
2025-03-20 21:48:37 +0100

    Timer finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('funguju!');
+const alarmElm = document.querySelector('.alarm');
+const audioElm = document.querySelector('audio');
+const ringTime = Number(prompt('Čas v sekundách. Za jak dlouho má minutka zazvonit?')) * 1000
+    
+const ring = () => {
+
+    alarmElm.classList.add('alarm--ring');
+    audioElm.play();
+}
+
+const timer = setTimeout(ring, ringTime);
+
+const buttonElm = document.querySelector('.button');
+
+const cancelTimer = () => {
+    clearTimeout(timer);
+};
+
+buttonElm.addEventListener('click', cancelTimer);
